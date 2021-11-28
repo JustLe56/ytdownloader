a658690d9241bb427200556793f3fbf4018cdbde
Justin Le <jule@csumb.edu>
2021-11-28 10:39:36 -0800

    now downloads entire file as mp4 then splits the mp4 file; this fixes the 1kb first chapter bug

--- code.js
+++ code.js
@@ -28,6 +28,7 @@ app.get("/videoInfo",async function(request,response){
 });
 
 app.get("/download",async function(request,response){
+	console.log("starting download")
 	const videoURL = request.query.videoURL;
 	const itag = request.query.itag;
 	const format = request.query.format;
@@ -35,69 +36,87 @@ app.get("/download",async function(request,response){
 	const info = await ytdl.getInfo(videoURL);
 
 	response.header("Content-Disposition",'attachment;\ filename="video.'+format+'"');
-	console.log(info.videoDetails.chapters.length-1)
+	//console.log(info.videoDetails.chapters.length-1)
 	const numChapters = info.videoDetails.chapters.length-1;
 	var i = 0;
 	var conversionCount = 0;
 	var downloadStream = ytdl(videoURL,{filter: format => format.itag == itag})
-	console.log("All chapters: ")
-	for (i = 0; i <= numChapters; i++){
-		var currTitle = info.videoDetails.chapters[i].title;
-		var currStartTime = info.videoDetails.chapters[i].start_time; //given in seconds aka 125 = 00:02:05
-		if (i === numChapters){
-			var nextStartTime = info.videoDetails.lengthSeconds;
-		}
-		else{
-			var nextStartTime = info.videoDetails.chapters[i+1].start_time;
-		}
-		
-		var duration = nextStartTime-currStartTime;
-		var startTimeConv = new Date((currStartTime) * 1000).toISOString().substr(11, 8);
 
-		console.log("Starting conversion for "+currTitle+" at: "+startTimeConv+ "to " +duration);
-		ffmpeg(downloadStream)
-			.setFfmpegPath(pathToFfmpeg)
-      		.setFfprobePath(ffprobe.path)
-			.setStartTime(startTimeConv) //start time of video (hh:mm:ss format)
-			.setDuration(duration) //duration of video (seconds format)
-			.withVideoCodec('copy')
-      		.withAudioCodec('copy')
-			.output('video_'+i+'.mp4')
-			.on('end', function(err) {
-				if(!err) { 
-					console.log('Conversion finished ' +conversionCount+ "vs "+numChapters); //TODO: figure out better way of signalling to compress than checking every time
-					
-					//console.log(conversionCount+"vs "+numChapters);
-					if (conversionCount === numChapters){
-						for (var j = 0; j <= numChapters; j++){
-							zip.addLocalFile("video_"+j+".mp4"); //TODO: change naming convention to use original chapter title
-						}
-						var zipFileContents = zip.toBuffer();
-						
-						//clean up local files
-						for (var j = 0; j <= numChapters; j++){
-							fs.unlink("video_"+j+".mp4", (err) =>{
-								if(err)throw err;
-								console.log("file deleted")
-							})
-						}
-
-						const fileName = 'uploads.zip';
-   						const fileType = 'application/zip';
-						response.writeHead(200, {
-							'Content-Disposition': `attachment; filename="${fileName}"`,
-							'Content-Type': fileType,
-						  })
-						return response.end(zipFileContents)
+	ffmpeg(ytdl(videoURL,{
+		filter: format => format.itag == itag
+	}))
+		.output('raw_download_out.mp4')
+		.on('end', function(err) {
+			if(!err) 
+			{ 
+				console.log("All chapters: ")
+				for (i = 0; i <= numChapters; i++){
+					var currTitle = info.videoDetails.chapters[i].title;
+					var currStartTime = info.videoDetails.chapters[i].start_time; //given in seconds aka 125 = 00:02:05
+					if (i === numChapters){
+						var nextStartTime = info.videoDetails.lengthSeconds;
 					}
-					conversionCount+=1;
+					else{
+						var nextStartTime = info.videoDetails.chapters[i+1].start_time;
+					}
+					
+					var duration = nextStartTime-currStartTime;
+					var startTimeConv = new Date((currStartTime) * 1000).toISOString().substr(11, 8);
+
+					console.log("Starting conversion for "+currTitle+" at: "+startTimeConv+ "to " +duration);
+					ffmpeg('raw_download_out.mp4')
+						.setFfmpegPath(pathToFfmpeg)
+						.setFfprobePath(ffprobe.path)
+						.setStartTime(startTimeConv) //start time of video (hh:mm:ss format)
+						.setDuration(duration) //duration of video (seconds format)
+						.withVideoCodec('copy') //mp4 specific
+						.withAudioCodec('copy') //mp4 specific
+						.output('video_'+i+'.mp4') //mp4 specific
+						// .noVideo() //mp3 specific 
+						// .format(`mp3`) //mp3 specific 
+						//.output('video_'+i+'.mp3') //mp3 specific 
+						.on('end', function(err) {
+							if(!err) { 
+								console.log('Conversion finished ' +conversionCount+ "vs "+numChapters); //TODO: figure out better way of signalling to compress than checking every time
+								
+								//console.log(conversionCount+"vs "+numChapters);
+								if (conversionCount === numChapters){
+									for (var j = 0; j <= numChapters; j++){
+										zip.addLocalFile("video_"+j+".mp4"); //check type before running
+									}
+									var zipFileContents = zip.toBuffer();
+									
+									//clean up local files
+									for (var j = 0; j <= numChapters; j++){
+										fs.unlink("video_"+j+".mp4", (err) =>{ //check type before running
+											if(err)throw err;
+											console.log("file deleted")
+										})
+									}
+
+									const fileName = 'uploads.zip';
+									const fileType = 'application/zip';
+									response.writeHead(200, {
+										'Content-Disposition': `attachment; filename="${fileName}"`,
+										'Content-Type': fileType,
+										})
+									return response.end(zipFileContents)
+								}
+								conversionCount+=1;
+							}
+							
+						})
+						.on('error', function(err){
+							console.log('error: ', err)
+						}).run()
 				}
-				
-			})
-			.on('error', function(err){
-				console.log('error: ', err)
-			}).run()
-	}
+			}
+		})
+		.on('error', function(err){
+			console.log('error: ', err)
+		}).run()	
+
+	
 
 });
 
